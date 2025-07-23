c80ce7286cd07c555715a84530288311e6e8829e
Baiju Thomas <baijubaby4880@gmail.com>
2025-07-23 19:45:34 +0530

    improved the feature by adding localstorage

--- dark_light_mode/script.js
+++ dark_light_mode/script.js
@@ -2,24 +2,45 @@ const fullDarkModeCheckbox = document.querySelector('#full-dark-mode');
 const containerDarkModeCheckbox = document.querySelector('#contained-dark-mode')
 const container = document.querySelector('.container');
 
+const isFullDarkMode = JSON.parse(localStorage.getItem('fullDarkModeCheckbox'));
+const isContainedDarkMode = JSON.parse(localStorage.getItem('containerDarkModeCheckbox'));
+
+if(isFullDarkMode){
+    fullDarkModeCheckbox.checked = true
+}
+if(isContainedDarkMode) {
+    containerDarkModeCheckbox.checked = true
+}
+
+    changeFullDarkModeCheckbox();
+    changeContainerDarkModeCheckbox();
+
+
 fullDarkModeCheckbox.addEventListener('change',()=>{
-    if(fullDarkModeCheckbox.checked){
-        document.body.classList.add('dark');
-    }else{
-       document.body.classList.remove('dark'); 
-    }
-    containerDarkModeCheckbox.checked = fullDarkModeCheckbox.checked;
-    if(containerDarkModeCheckbox.checked){
-        container.classList.add('dark');
-    }else{
-       container.classList.remove('dark');  
-    }
+    changeFullDarkModeCheckbox()
+    changeContainerDarkModeCheckbox();
+   
 });
 
-containerDarkModeCheckbox.addEventListener('change',()=>{
+function changeFullDarkModeCheckbox(){
+  if(fullDarkModeCheckbox.checked){
+        document.body.classList.add('dark');
+        localStorage.setItem("fullDarkModeCheckbox",true);
+    }else{
+       document.body.classList.remove('dark'); 
+        localStorage.setItem("fullDarkModeCheckbox",false);
+    }
+}
+
+function changeContainerDarkModeCheckbox() {
+//  containerDarkModeCheckbox.checked = fullDarkModeCheckbox.checked;
     if(containerDarkModeCheckbox.checked){
         container.classList.add('dark');
+         localStorage.setItem("containerDarkModeCheckbox",true);
     }else{
        container.classList.remove('dark');  
+        localStorage.setItem("containerDarkModeCheckbox",false);
     }
-})
+}
+
+containerDarkModeCheckbox.addEventListener('change',changeContainerDarkModeCheckbox)
