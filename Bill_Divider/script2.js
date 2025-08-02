476b873cdcd01e627bf55b7aae35805dbc962a79
Baiju Thomas <baijubaby4880@gmail.com>
2025-08-02 21:40:08 +0530

    Adding some updates on the splitter file

--- Bill_Divider/script2.js
+++ Bill_Divider/script2.js
@@ -5,15 +5,16 @@ const tipInputField = document.querySelector('.custom-tip');
 const numberOfPeople = document.querySelector('.number-of-people');
 const generateBill = document.querySelector('.generate-bill-btn');
 
-const tipAmount = document.querySelector('.tip-amount span');
+const tipAmountM = document.querySelector('.tip-amount span');
 const finalAmount = document.querySelector('.total span');
 const eachPersonAmount = document.querySelector('.each-person-bill span');
 const resetButton = document.querySelector('.reset-btn');
 
+
 let selectedTip = '';
 
 amountInputField.addEventListener('input', ()=>{
-    let initialAmount = amountInputField.value;
+   let initialAmount = amountInputField.value;
     if(initialAmount >=0){
         tipContainer.classList.remove('disabled');
         tipInputField.disabled = false;
@@ -23,11 +24,35 @@ amountInputField.addEventListener('input', ()=>{
 });
 
 tipButton.forEach(button =>{
-    button.addEventListener('click',(e)=>{
-     selectedTip =e.target;
-    
-     
-     
-})
-})
+    button.addEventListener('click',()=>{ 
+     tipButton.forEach(btn => btn.classList.remove('selected'));
+     button.classList.add('selected');
+     selectedTip = button.textContent.replace('%','');
+     tipInputField.value='';
+});
+});
 
+tipInputField.addEventListener('input',()=>{
+    selectedTip =  tipInputField.value;
+    tipButton.forEach(btn => btn.classList.remove('selected'));
+});
+
+generateBill.addEventListener('click',()=>{
+    let initialAmount = parseFloat(amountInputField.value)
+    let eachPersonBill = parseInt(numberOfPeople.value)
+
+    let tipAmount = (initialAmount*selectedTip)/100
+    console.log(tipAmount);
+    let total = tipAmount + initialAmount; 
+    console.log(total);
+    let split = total/eachPersonBill;
+    console.log(split);
+
+    
+
+    tipAmountM.textContent = `${tipAmount.toFixed(2)}`;
+    finalAmount.textContent = `${total.toFixed(2)}`;
+    eachPersonAmount.textContent = `${split.toFixed(2)}`;
+
+    resetButton.disabled = false;
+});
